57518ce69a6b8ae404e4b6509d39a9cfa34a27d5
yushan <yushanwebdev@gmail.com>
2022-04-24 20:04:51 +0530

    feat: Provide Testing Helper Functions as Globals in JavaScript

--- setup-globals.js
+++ setup-globals.js
@@ -1,10 +1,10 @@
 async function test(title, callback) {
   try {
-    await callback()
-    console.log(`✓ ${title}`)
+    await callback();
+    console.log(`✓ ${title}`);
   } catch (error) {
-    console.error(`✕ ${title}`)
-    console.error(error)
+    console.error(`✕ ${title}`);
+    console.error(error);
   }
 }
 
@@ -12,11 +12,11 @@ function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
+        throw new Error(`${actual} is not equal to ${expected}`);
       }
-    }
-  }
+    },
+  };
 }
 
-global.test = test
-global.expect = expect
+global.test = test;
+global.expect = expect;
