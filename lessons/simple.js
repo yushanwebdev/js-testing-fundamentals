1e4492dc63d4e3124a8a390c4058cc27fa7da0db
yushan <yushanwebdev@gmail.com>
2022-04-24 15:24:41 +0530

    feat: Throw an Error with a Simple Test in JavaScript

--- lessons/simple.js
+++ lessons/simple.js
@@ -1,15 +1,15 @@
-const {sum, subtract} = require('../math')
+const { sum, subtract } = require("../math");
 
-let result, expected
+let result = sum(3, 7);
+let expected = 10;
 
-result = sum(3, 7)
-expected = 10
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
 
-result = subtract(7, 3)
-expected = 4
+result = subtract(7, 3);
+expected = 4;
+
 if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`)
+  throw new Error(`${result} is not equal to ${expected}`);
 }
