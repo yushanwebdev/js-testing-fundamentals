952c5425d1210ae192ef3bcbe82576292789e89f
yushan <yushanwebdev@gmail.com>
2022-04-24 16:31:54 +0530

    feat: Support Async Tests with JavaScripts Promises through async await

--- lessons/async-await.js
+++ lessons/async-await.js
@@ -1,24 +1,24 @@
-const {sumAsync, subtractAsync} = require('../math')
+const { sumAsync, subtractAsync } = require("../math");
 
-test('sumAsync adds numbers asynchronously', async () => {
-  const result = await sumAsync(3, 7)
-  const expected = 10
-  expect(result).toBe(expected)
-})
+test("sum adds numbers", async () => {
+  const result = await sumAsync(3, 7);
+  const expected = 10;
+  expect(result).toBe(expected);
+});
 
-test('subtractAsync subtracts numbers asynchronously', async () => {
-  const result = await subtractAsync(7, 3)
-  const expected = 4
-  expect(result).toBe(expected)
-})
+test("subtract subtracts numbers", async () => {
+  const result = await subtractAsync(7, 3);
+  const expected = 4;
+  expect(result).toBe(expected);
+});
 
 async function test(title, callback) {
   try {
-    await callback()
-    console.log(`✓ ${title}`)
+    await callback();
+    console.log(`\u2713 ${title}`);
   } catch (error) {
-    console.error(`✕ ${title}`)
-    console.error(error)
+    console.error(`x ${title}`);
+    console.error(error);
   }
 }
 
@@ -26,8 +26,10 @@ function expect(actual) {
   return {
     toBe(expected) {
       if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
+        throw new Error(`${actual} is not equal to ${expected}`);
       }
-    }
-  }
+    },
+    toEqual(expected) {},
+    toBeGreaterThan(expected) {},
+  };
 }
